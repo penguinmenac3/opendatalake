bf7ab193e394f54b5d2bd787d6458277a15d5c22
Michael Fuerst <fuerst@fzi.de>
2018-06-12 11:29:31 +0200

    Improved kitti initial load up time to speed up development time.

--- opendatalake/detection/kitti_detection.py
+++ opendatalake/detection/kitti_detection.py
@@ -13,11 +13,23 @@ PHASE_VALIDATION = "validation"
 
 
 def _gen(params, stride=1, offset=0, infinite=False):
-    images, calibrations, data_split, phase, data = params
+    filenames, data_split, phase, base_dir = params
 
     loop_condition = True
     while loop_condition:
-        for idx in range(offset, len(images), stride):
+        for idx in range(offset, len(filenames), stride):
+            filename = filenames[idx]
+            image = os.path.join(base_dir, "data_object_image_2", "training", "image_2",
+                                       filename.replace(".txt", ".png"))
+
+            calibration_file = os.path.join(base_dir, "data_object_calib", "training", "calib", filename)
+            calibration = np.genfromtxt(calibration_file, delimiter=' ', usecols=range(1, 13), skip_footer=3)
+            calibration = calibration[2].reshape(3, 4)
+
+            with open(os.path.join(base_dir, "training", "label_2", filename), 'r') as myfile:
+                data = myfile.read().strip().split("\n")
+                # Pedestrian 0.00 0 -0.20 712.40 143.00 810.73 307.92 1.89 0.48 1.20 1.84 1.47 8.41 0.01
+
             if data_split and idx % data_split == 0 and phase == PHASE_TRAIN:
                 continue
 
@@ -27,7 +39,7 @@ def _gen(params, stride=1, offset=0, infinite=False):
             detections2d = []
             detections25d = []
 
-            for anno in data[idx]:
+            for anno in data:
                 date = anno.split(" ")
                 if int(date[2]) == -1:
                     continue
@@ -40,39 +52,23 @@ def _gen(params, stride=1, offset=0, infinite=False):
                 # Calc cx and cy in image coordinates.
                 translation = [float(x) for x in date[11:14]]
                 center = np.array(translation) + np.array([0.0, -float(date[8]) / 2.0, 0])
-                projected_center = apply_projection(center, calibrations[idx])
+                projected_center = apply_projection(center, calibration)
                 dist = vec_len(center)
                 detections25d.append(Detection25d(class_id=date[0],
                                                     cx=projected_center[0][0], cy=projected_center[1][0], dist=dist,
                                                     w=float(date[9]), h=float(date[8]), l=float(date[10]),
                                                     theta=float(date[3])))
 
-            feature = imread(images[idx], mode="RGB")
-            yield ({"image": feature, "calibration": calibrations[idx]},
+            feature = imread(image, mode="RGB")
+            yield ({"image": feature, "calibration": calibration},
                    {"detections_2d": detections2d, "detections_2.5d": detections25d})
         loop_condition = infinite
 
 
 def kitti_detection(base_dir, phase, data_split=10):
-    images = []
-    calibrations = []
-    features = []
+    filenames = [f for f in os.listdir(os.path.join(base_dir, "training", "label_2")) if f.endswith(".txt")]
 
-    for filename in os.listdir(os.path.join(base_dir, "training", "label_2")):
-        if filename.endswith(".txt"):
-            images.append(os.path.join(base_dir, "data_object_image_2", "training", "image_2", filename.replace(".txt", ".png")))
-
-            calibration_file = os.path.join(base_dir, "data_object_calib", "training", "calib", filename)
-            calibration_matrix = np.genfromtxt(calibration_file, delimiter=' ', usecols=range(1, 13), skip_footer=3)
-            calibration_matrix = calibration_matrix[2].reshape(3, 4)
-            calibrations.append(calibration_matrix)
-
-            with open(os.path.join(base_dir, "training", "label_2", filename), 'r') as myfile:
-                data = myfile.read().strip().split("\n")
-                # Pedestrian 0.00 0 -0.20 712.40 143.00 810.73 307.92 1.89 0.48 1.20 1.84 1.47 8.41 0.01
-                features.append(data)
-
-    return _gen, (images, calibrations, data_split, phase, features)
+    return _gen, (filenames, data_split, phase, base_dir)
 
 
 def evaluate3d(predictor, prediction_2_detections, base_dir, visualize=False):
