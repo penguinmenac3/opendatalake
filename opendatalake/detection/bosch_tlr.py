9fbe11ed8288caa70460cdeeaebb18d3d053e3b8
Michael Fuerst <mail@michaelfuerst.de>
2018-09-02 10:21:26 +0200

    Started porting over to be compatible with keras.

--- opendatalake/detection/bosch_tlr.py
+++ opendatalake/detection/bosch_tlr.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import yaml
+import math
 
 import numpy as np
 from scipy.misc import imread
+from keras.utils import Sequence
 
 from opendatalake.detection.utils import Detection2d
 
@@ -24,14 +26,39 @@ COLOR_MAP = {
     "Ignore": (0, 0, 255)
 }
 
+class BoschTLR(Sequence):
+    def __init__(self, hyperparams, base_dir, phase, riib=False, preprocess_feature=None, preprocess_label=None, augment_data=None):
+        input_yaml = "train.yaml" if phase == PHASE_TRAIN else "test.yaml"
 
-def _gen(params, stride=1, offset=0, infinite=False):
-    data = params
+        images = yaml.load(open(os.path.join(base_dir, input_yaml), 'rb').read())
 
-    loop_condition = True
-    while loop_condition:
-        for idx in range(offset, len(data), stride):
-            date = data[idx]
+        for i in range(len(images)):
+            if phase == PHASE_VALIDATION:
+                images[i]['path'] = os.path.join(base_dir, "rgb", "test", images[i]['path'].split("/")[-1])
+            else:
+                images[i]['path'] = os.path.join(base_dir, images[i]['path'])
+            if riib:
+                images[i]['path'] = images[i]['path'].replace('.png', '.pgm')
+                images[i]['path'] = images[i]['path'].replace('rgb/train', 'riib/train')
+                images[i]['path'] = images[i]['path'].replace('rgb/test', 'riib/test')
+                for box in images[i]['boxes']:
+                    box['y_max'] = box['y_max'] + 8
+                    box['y_min'] = box['y_min'] + 8
+        self.images = images
+        self.hyperparams = hyperparams
+        self.batch_size = hyperparams["train"]["batch_size"]
+        self.preprocess_feature = preprocess_feature
+        self.preprocess_label = preprocess_label
+        self.augment_data = augment_data
+
+    def __len__(self):
+        return math.ceil(len(self.images)/self.batch_size)
+
+    def __getitem__(self, index):
+        features = []
+        labels = []
+        for idx in range(index * self.batch_size, min((index + 1) * self.batch_size, len(self))):
+            date = self.images[idx]
             feature = imread(date['path'], mode="RGB")
             detections2d = []
 
@@ -45,52 +72,14 @@ def _gen(params, stride=1, offset=0, infinite=False):
                                                 cy=(box['y_min'] + box['y_max']) / 2.0,
                                                 w=box['x_max'] - box['x_min'],
                                                 h=box['y_max'] - box['y_min']))
-
-            yield ({"image": feature},
-                   {"detections_2d": detections2d})
-        loop_condition = infinite
-
-
-def bosch_tlr(base_dir, phase, riib=False):
-    input_yaml = "train.yaml" if phase == PHASE_TRAIN else "test.yaml"
-
-    images = yaml.load(open(os.path.join(base_dir, input_yaml), 'rb').read())
-
-    for i in range(len(images)):
-        if phase == PHASE_VALIDATION:
-            images[i]['path'] = os.path.join(base_dir, "rgb", "test", images[i]['path'].split("/")[-1])
-        else:
-            images[i]['path'] = os.path.join(base_dir, images[i]['path'])
-        if riib:
-            images[i]['path'] = images[i]['path'].replace('.png', '.pgm')
-            images[i]['path'] = images[i]['path'].replace('rgb/train', 'riib/train')
-            images[i]['path'] = images[i]['path'].replace('rgb/test', 'riib/test')
-            for box in images[i]['boxes']:
-                box['y_max'] = box['y_max'] + 8
-                box['y_min'] = box['y_min'] + 8
-
-    return _gen, images
-
-
-if __name__ == "__main__":
-    import cv2
-
-    print("Loading Dataset:")
-    train_data = bosch_tlr("datasets/bosch_tlr", phase=PHASE_TRAIN)
-
-    data_fn, data_params = train_data
-    data_gen = data_fn(data_params)
-
-    for feat, label in data_gen:
-        img = feat["image"].copy()
-        if len(label["detections_2d"]) < 1:
-            continue
-        for detection in label["detections_2d"]:
-            color = COLOR_MAP["Ignore"]
-            if detection.class_id in COLOR_MAP:
-                color = COLOR_MAP[detection.class_id]
-            else:
-                print("Unknown color: {}".format(detection.class_id))
-            detection.visualize(img, color=color)
-        cv2.imshow("Bosch TLR", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-        key = cv2.waitKey(500)
+            feature = {"image": feature}
+            label = {"detections_2d": detections2d}
+            if self.augment_data is not None:
+                feature, label = self.augment_data(self.hyperparams, feature, label)
+            if self.preprocess_feature is not None:
+                feature = self.preprocess_feature(self.hyperparams, feature)
+            if self.preprocess_label is not None:
+                label = self.preprocess_label(self.hyperparams, feature, label)
+            features.append(feature)
+            labels.append(label)
+        return ({k: [dic[k] for dic in features] for k in features[0]}, {k: [dic[k] for dic in labels] for k in labels[0]})
